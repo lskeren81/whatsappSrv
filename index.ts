b1ee3253acb741135558eebbb02c3463ac0ef781
david2 <101044543+lskeren81@users.noreply.github.com>
2025-05-20 15:13:29 +0000

    progress

--- index.ts
+++ index.ts
@@ -1,4 +1,34 @@
 import { Hono } from 'hono'
+import { makeWASocket,useMultiFileAuthState } from 'baileys';
+import qrcode from "qrcode-terminal";
+
+const { state, saveCreds } = await useMultiFileAuthState('auth_info')
+const sock = makeWASocket({
+  auth: state,
+  syncFullHistory: false
+})
+sock.ev.on('creds.update', saveCreds)
+
+sock.ev.on("connection.update", handleConnectionUpdate);
+async function handleConnectionUpdate(update: any) {
+    const { qr, connection, lastDisconnect } = update;
+    if (qr) {
+        qrcode.generate(qr, { small: true });
+        console.log("Scan the QR code above to log in to WhatsApp.");
+    }
+    if (connection === "close") {
+        const shouldReconnect = lastDisconnect?.error?.output?.statusCode !== 401;
+        console.log("Connection closed. Reconnecting...", shouldReconnect);
+        if (shouldReconnect) {
+            const { state, saveCreds } = await useMultiFileAuthState("auth_info_baileys");
+            const newSock = makeWASocket({ syncFullHistory: false, auth: state });
+            newSock.ev.on("creds.update", saveCreds);
+            newSock.ev.on("connection.update", handleConnectionUpdate);
+        } else {
+            console.log("Session ended. Delete auth_info_baileys and restart to generate a new QR.");
+        }
+    }
+}
 
 const app = new Hono()
 
